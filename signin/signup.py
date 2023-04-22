8e0bd3db4b98b9a6df856d94d4a782bab4e5196d
Alejandro <delcastillo.diaz.20067@itsmante.edu.mx>
2023-04-22 12:22:34 -0600

    Añadida pantalla de registro

--- signin/signup.py
+++ signin/signup.py
@@ -5,9 +5,9 @@ from kivy.lang import Builder
 
 from sqlqueries import QueriesSQLite
 
-Builder.load_file('signin/signin.kv')
+Builder.load_file('signin/signup.kv')
 
-class SigninWindow(BoxLayout):
+class SignupWindow(BoxLayout):
 	def __init__(self, poner_usuario_callback, **kwargs):
 		super().__init__(*kwargs)
 		self.poner_usuario=poner_usuario_callback
@@ -17,7 +17,7 @@ class SigninWindow(BoxLayout):
 		users=QueriesSQLite.execute_read_query(connection, "SELECT * from usuarios")
 		if users:
 			if username=='' or password=='':
-				self.ids.signin_notificacion.text='Falta nombre de usuario y/o contraseña'
+				self.ids.signup_notificacion.text='Falta nombre de usuario y/o contraseña'
 			else:
 				usuario={}
 				for user in users:
@@ -41,30 +41,50 @@ class SigninWindow(BoxLayout):
 
 						self.ids.username.text=''
 						self.ids.password.text=''
-						self.ids.signin_notificacion.text=''
+						self.ids.signup_notificacion.text=''
 						if usuario['tipo']=='trabajador':
 							self.parent.parent.current='scrn_ventas'
 						else:
 							self.parent.parent.current='scrn_admin'
 						self.poner_usuario(usuario)
 					else:
-						self.ids.signin_notificacion.text='Usuario o contraseña incorrecta'
+						self.ids.signup_notificacion.text='Usuario o contraseña incorrecta'
 				else:
-					self.ids.signin_notificacion.text='Usuario inexistente'
+					self.ids.signup_notificacion.text='Usuario inexistente'
 		else:
 			usuario_tuple=('usuario', 'Usuario Inicio', '123', 'admin')
 			crear_usuario = "INSERT INTO usuarios (username, nombre, password, tipo) VALUES (?,?,?,?);"
 			QueriesSQLite.execute_query(connection, crear_usuario, usuario_tuple)
-			self.ids.signin_notificacion.text='Se creo primer usuario. usuario 123'
+			self.ids.signup_notificacion.text='Se creo primer usuario. usuario 123'
 
 	def crear_usuario(self, username, password):
-		self.parent.parent.current='scrn_signup'
+		connection = QueriesSQLite.create_connection("pdvDB.sqlite")
+		users=QueriesSQLite.execute_read_query(connection, "SELECT * from usuarios")
+		if users:
+			if username=='' or password=='':
+				self.ids.signup_notificacion.text='Falta nombre de usuario y/o contraseña'
+			else:
+				usuario={}
+				for user in users:
+					if user[0]==username:
+						usuario['nombre']=user[1]
+						usuario['username']=user[0]
+						usuario['password']=user[2]
+						usuario['tipo']=user[3]
+						break
+				if usuario:
+						self.ids.signup_notificacion.text='El usuario ya existe'
+				else:
+					usuario_tuple=(username, 'Usuario prueba', password, 'admin')
+					crear_usuario = "INSERT INTO usuarios (username, nombre, password, tipo) VALUES (?,?,?,?);"
+					QueriesSQLite.execute_query(connection, crear_usuario, usuario_tuple)
+					self.ids.signup_notificacion.text='Se creo el usuario'
 
 
 
-class SigninApp(App):
+class SignupApp(App):
 	def build(self):
-		return SigninWindow()
+		return SignupWindow()
 
 if __name__=="__main__":
-	SigninApp().run()
+	SignupApp().run()
